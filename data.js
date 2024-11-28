a2802eac72e164fcb23ac911a79e3349d8d2c5aa
Lokavit <491262164@qq.com>
2024-11-28 19:19:13 +0800

    -c init

---
+++ data.js
@@ -0,0 +1,161 @@
+/** 
+FileName: data.js
+Birthtime: 2024/11/28 18:40:55
+Mtime: 2024/11/28 18:53:58
+-----
+Copyright © 1911 - 2024 UserName
+    卍 · 小僧過境　衆生甦醒 · 卍
+=====<< 卍 · Description · 卍 >>=====
+*/
+
+export const enUS = {
+  code: "en-US", // 美
+  lang: "English",
+  timeZone: "America/New_York",
+  flag: "us",
+  currency: "USD",
+};
+
+export const deDE = {
+  code: "de-DE", // 简中
+  lang: "Deutsch",
+  timeZone: "Europe/Berlin",
+  flag: "de",
+  currency: "EUR",
+};
+
+export const zhCN = {
+  code: "zh-CN", // 简中
+  lang: "中文简体",
+  timeZone: "Asia/Shanghai",
+  flag: "cn",
+  currency: "CNY",
+};
+
+export const zhTW = {
+  code: "zh-TW", // 繁中
+  lang: "台湾繁体",
+  timeZone: "Asia/Taipei",
+  flag: "tw",
+  currency: "TWD",
+};
+
+export const frFR = {
+  code: "fr-FR", // 法
+  lang: "Français",
+  timeZone: "Europe/Paris",
+  flag: "fr",
+  currency: "EUR",
+};
+
+export const itIT = {
+  code: "it-IT", // 意大利
+  lang: "Italiano",
+  timeZone: "Europe/Rome",
+  flag: "it",
+  currency: "EUR",
+};
+
+export const jaJP = {
+  code: "ja-JP", // 日
+  lang: "日本語",
+  timeZone: "Asia/Tokyo",
+  flag: "jp",
+  currency: "JPY",
+};
+
+export const thTH = {
+  code: "th-TH", // 泰
+  lang: "ไทย",
+  timezone: "Asia/Bangkok",
+  flag: "th",
+  currency: "THB",
+};
+
+export const ptPT = {
+  code: "pt-PT", // 葡萄牙
+  lang: "Português",
+  timeZone: "Europe/Lisbon",
+  flag: "pt",
+  currency: "EUR",
+};
+
+export const esES = {
+  code: "es-ES", // 西班牙
+  lang: "Español",
+  timeZone: "Europe/Madrid",
+  flag: "es",
+  currency: "EUR",
+};
+
+export const koKR = {
+  code: "ko-KR", // 韩
+  lang: "한국의",
+  timeZone: "Asia/Seoul",
+  flag: "kr",
+  currency: "KRW",
+};
+
+export const trTR = {
+  code: "tr-TR", // 土耳其
+  lang: "Türkçe",
+  timeZone: "Europe/Istanbul",
+  flag: "tr",
+  currency: "TRY",
+};
+
+export const arIL = {
+  code: "ar-IL", // 阿拉伯语（以色列)
+  lang: "العربية",
+  timezone: "Asia/Jerusalem",
+  flag: "il",
+  currency: "SAR",
+};
+
+export const viVN = {
+  code: "vi-VN", // 越南语
+  lang: "tiếng việt",
+  timeZone: "Asia/Ho_Chi_Minh",
+  flag: "vn",
+  currency: "VND",
+};
+
+export const ruRU = {
+  code: "ru-RU", // 俄
+  lang: "Русский",
+  timeZone: "Europe/Moscow",
+  flag: "ru",
+  currency: "RUB",
+};
+
+export const enGB = {
+  code: "en-GB", // 英国
+  lang: "English（United Kingdom)",
+  timeZone: "Europe/London",
+  flag: "gb",
+  currency: "",
+};
+
+export const enAU = {
+  code: "en-AU", // 澳大利亚
+  lang: "English（Australia)",
+  timeZone: "Australia/Sydney",
+  flag: "au",
+  currency: "",
+};
+
+export const bnIN = {
+  code: "bn-IN", // 孟加拉语（印度)
+  lang: "বাংলা",
+  timeZone: "Asia/Dhaka",
+  flag: "bn",
+  currency: "",
+};
+
+export const hiIN = {
+  code: "hi-IN", // 印度语
+  lang: "हिंदी",
+  timeZone: "Asia/Kolkata",
+  flag: "hi",
+  currency: "",
+};
---
+++ index.js
@@ -0,0 +1,53 @@
+/** 
+FileName: index.js
+Birthtime: 2024/11/28 18:40:19
+Mtime: 2024/11/28 18:59:12
+-----
+Copyright © 1911 - 2024 UserName
+    卍 · 小僧過境　衆生甦醒 · 卍
+=====<< 卍 · Description · 卍 >>=====
+*/
+
+import { enUS } from "./data";
+import { zhCN } from "./data";
+import { zhTW } from "./data";
+import { deDE } from "./data";
+import { frFR } from "./data";
+import { itIT } from "./data";
+import { jaJP } from "./data";
+import { thTH } from "./data";
+import { ptPT } from "./data";
+import { esES } from "./data";
+import { koKR } from "./data";
+import { trTR } from "./data";
+import { arIL } from "./data";
+import { viVN } from "./data";
+import { ruRU } from "./data";
+import { enGB } from "./data";
+import { enAU } from "./data";
+import { bnIN } from "./data";
+import { hiIN } from "./data";
+
+const lgre = {
+  enUS,
+  zhCN,
+  zhTW,
+  deDE,
+  frFR,
+  itIT,
+  jaJP,
+  thTH,
+  ptPT,
+  esES,
+  koKR,
+  trTR,
+  arIL,
+  viVN,
+  ruRU,
+  enGB,
+  enAU,
+  bnIN,
+  hiIN,
+};
+
+export default lgre;
